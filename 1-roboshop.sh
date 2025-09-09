d7f5f27ff79956c20143e2dbb723a73bbb2014c9
Naveen G <gangineninaveen132@gmail.com>
2025-09-09 13:28:33 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -25,15 +25,35 @@ do
     INSTACE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0d8d7189bee7912bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query 'Instances[0].InstanceId' --output text)  # have instance id is replaced in the place of private ip, due to which public ip need to be query in this soo
     if [ $instance != "frontend" ]
     then
-        PRIVATE_IP=$(aws ec2 describe-instances --instance-ids $INSTACE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        echo "$instance PRIVATE IP Address : $PRIVATE_IP"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTACE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        echo "$instance PRIVATE IP Address : $IP"
     else
-        PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTACE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        echo "$instance PUBLIC IP Address : $PUBLIC_IP"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTACE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        echo "$instance PUBLIC IP Address : $IP"
 
     fi
-    echo "$instance IP Address is public : $PUBLIC_IP"
-    echo "$instance IP Address is private : $PRIVATE_IP"
+    echo "$instance IP Address is public : $IP"
+    echo "$instance IP Address is private : $IP"
 
 done
 
+
+# Updating or creating records route 53 through aws cli  ->  its in stack over flow take the code from it and update records
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating or updating record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$Instance' . '$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
